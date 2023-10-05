83f4c678fa04ae3ab34490ddd71856ea3102447e
Sachin-840480 <sachin1712003@gmail.com>
2023-10-05 08:48:53 +0530

    Updated the main file better readability

--- face_recognition.py
+++ face_recognition.py
@@ -7,21 +7,27 @@ from time import time
 # model_path = r"C:/Users/{Sachin}/AppData/Local/Programs/Python/Python311/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"
 face_cap = cv2.CascadeClassifier("./Facial Recognition/models/haarcascade_frontalface_default.xml")
 
+
 #Creating the Video Capturing Object.
 video_cap = cv2.VideoCapture(0)
+
 
 #Initialize a variable to keep track of the "face counter".
 face_counter = 0
 
+
 #Initialize a variable to keep track of "Last Capture Time".
 last_capture_time = 0
+
 
 #Running the Camera All-Time.
 while True:
     ret, video_data = video_cap.read()
 
+
     #Converting the Captured Face into 'greyscale'.
     grey = cv2.cvtColor(video_data, cv2.COLOR_BGR2GRAY)
+
 
     #Captuing the 'faces'.
     faces = face_cap.detectMultiScale(
@@ -32,6 +38,7 @@ while True:
         flags = cv2.CASCADE_SCALE_IMAGE
         )
 
+
     #Creating the 'Rectangle around the face'.
     for (x, y, w, h) in faces:
         
@@ -41,11 +48,14 @@ while True:
 
             cv2.rectangle(video_data, (x,y), (x + w, y + h), (0,255,0),2)
 
+
             #Crop the detected face.
             cropped_face = video_data[y : y+h, x : x+w]
 
+
             #Update the last_capture_time.
             last_capture_time = current_time
+
 
             #Save the cropped face with a 'unique' name.
             face_counter += 1
@@ -53,12 +63,15 @@ while True:
             cv2.imwrite(face_filename, cropped_face)
             print(f'Saved: {face_filename}')
 
+
     #Display the frame with detected faces.
     cv2.imshow("Video_Live", video_data)
+
 
     #Exit the loop if the 'a' key is pressed.
     if cv2.waitKey(10) == ord('a'):
         break
+
 
 #Release the camera and close all OpenCV windows.
 video_cap.release()
